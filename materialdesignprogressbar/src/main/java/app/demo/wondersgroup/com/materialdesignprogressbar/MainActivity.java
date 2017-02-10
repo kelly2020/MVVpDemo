a77d021ce5dad510dc376e307302f155caacbfa3
zhangwentao <zhangwentao@wondersgroup.com>
2017-02-10 16:59:19 +0800

    添加了水显示的进度

---
+++ materialdesignprogressbar/src/main/java/app/demo/wondersgroup/com/materialdesignprogressbar/MainActivity.java
@@ -0,0 +1,31 @@
+package app.demo.wondersgroup.com.materialdesignprogressbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import me.zhanghai.android.materialprogressbar.MaterialProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        final MaterialProgressBar materialProgressBar = (MaterialProgressBar) findViewById(R.id.progressbar_material);
+//        Button button = (Button) findViewById(R.id.button_stop_progress);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                materialProgressBar.setVisibility(View.GONE);
+//            }
+//        });
+
+
+
+    }
+}
